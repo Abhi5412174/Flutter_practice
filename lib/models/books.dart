dfbbc25c25441ac0548e1f3f24163dbcefca0727
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2024-09-22 22:50:19 +0545

    day8 completed

---
+++ lib/models/books.dart
@@ -0,0 +1,29 @@
+// import 'dart:math';
+
+class Item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final num credit;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.credit,
+      required this.image});
+}
+
+final books = [
+  Item(
+      id: 001,
+      name: "demo_book1",
+      desc: "this is a book.",
+      price: 500,
+      credit: 5,
+      image:
+          "https://exponentwptheme.com/book/wp-content/uploads/sites/26/2019/02/book-mockup-exponent-min.png")
+];
